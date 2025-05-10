dbd6546e2baea418e575b40ced06189ff3106ad4
George Lucas Gomes <gomes.georgelucas@gmail.com>
2025-05-10 09:28:52 -0400

    Atividade Prática 4 – Fila: Refatorado código para utilizar uma fila do FreeRTOS como mecanismo de comunicação entre as tarefas de leitura do botão e controle do LED.

--- Firmware/HTNB32L-XXX-SDK/Applications/MQTT_Example/Src/main.c
+++ Firmware/HTNB32L-XXX-SDK/Applications/MQTT_Example/Src/main.c
@@ -16,93 +16,91 @@
 #include "main.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "queue.h"
 
-#ifndef CUSTOM_BUTTON_INSTANCE
-#define CUSTOM_BUTTON_INSTANCE   0
-#endif
-
-#ifndef CUSTOM_BUTTON_PIN
-#define CUSTOM_BUTTON_PIN        10
-#endif
-
-#ifndef CUSTOM_BUTTON_PAD_ID
-#define CUSTOM_BUTTON_PAD_ID     25
-#endif
-
-#ifndef BLUE_LED_INSTANCE
-#define BLUE_LED_INSTANCE        0
-#endif
-
-#ifndef BLUE_LED_PIN
-#define BLUE_LED_PIN             3
-#endif
-
-#ifndef BLUE_LED_PAD_ID
-#define BLUE_LED_PAD_ID          14
-#endif
-
-//#ifndef LED_ON
-#define LED_ON   0
-//#endif
-
-//#ifndef LED_OFF
-#define LED_OFF  1
-//#endif
+QueueHandle_t xButtonQueue;
 
 volatile bool button_pressed = false;
 
-static uint32_t uart_cntrl = (ARM_USART_MODE_ASYNCHRONOUS | ARM_USART_DATA_BITS_8 | ARM_USART_PARITY_NONE | 
-                                ARM_USART_STOP_BITS_1 | ARM_USART_FLOW_CONTROL_NONE);
+static uint32_t uart_cntrl = (ARM_USART_MODE_ASYNCHRONOUS | ARM_USART_DATA_BITS_8 | ARM_USART_PARITY_NONE |
+                              ARM_USART_STOP_BITS_1 | ARM_USART_FLOW_CONTROL_NONE);
 
 extern USART_HandleTypeDef huart1;
 
-
 void Task1(void *pvParameters) {
+    bool state;
     while (1) {
-        uint32_t state = HT_GPIO_PinRead(CUSTOM_BUTTON_INSTANCE, CUSTOM_BUTTON_PIN);
-        button_pressed = (state == 0); // Pressionado = nível baixo
+        state = (HT_GPIO_PinRead(CUSTOM_BUTTON_INSTANCE, CUSTOM_BUTTON_PIN) == 0); // pressionado = 0
+        xQueueSend(xButtonQueue, &state, portMAX_DELAY);
         vTaskDelay(pdMS_TO_TICKS(50));
     }
 }
 
 void Task2(void *pvParameters) {
+    bool received_state;
     while (1) {
-        HT_GPIO_WritePin(BLUE_LED_PIN, BLUE_LED_INSTANCE, button_pressed ? LED_ON : LED_OFF);
-        vTaskDelay(pdMS_TO_TICKS(50));
+        if (xQueueReceive(xButtonQueue, &received_state, portMAX_DELAY) == pdPASS) {
+            HT_GPIO_WritePin(BLUE_LED_PIN, BLUE_LED_INSTANCE, received_state ? LED_ON : LED_OFF);
+        }
     }
 }
+
+void init_button(void)
+{
+    pad_config_t padConfig;
+    gpio_pin_config_t gpioConfig;
+
+    PAD_GetDefaultConfig(&padConfig);
+    padConfig.mux = PAD_MuxAlt0;
+
+    PAD_SetPinConfig(CUSTOM_BUTTON_PAD_ID, &padConfig);
+    PAD_SetPinPullConfig(CUSTOM_BUTTON_PAD_ID, PAD_InternalPullUp);
+
+    gpioConfig.pinDirection = GPIO_DirectionInput;
+    gpioConfig.misc.interruptConfig = GPIO_InterruptDisabled;
+
+    GPIO_PinConfig(CUSTOM_BUTTON_INSTANCE, CUSTOM_BUTTON_PIN, &gpioConfig);
+}
+
+void init_led(void)
+{
+    pad_config_t padConfig;
+    gpio_pin_config_t gpioConfig;
+
+    PAD_GetDefaultConfig(&padConfig);
+    padConfig.mux = PAD_MuxAlt0;
+
+    PAD_SetPinConfig(BLUE_LED_PAD_ID, &padConfig);
+    PAD_SetPinPullConfig(BLUE_LED_PAD_ID, PAD_AutoPull);
+
+    gpioConfig.pinDirection = GPIO_DirectionOutput;
+    gpioConfig.misc.initOutput = LED_OFF;
+
+    GPIO_PinConfig(BLUE_LED_INSTANCE, BLUE_LED_PIN, &gpioConfig);
+}
+
 
 /**
   \fn          int main_entry(void)
   \brief       main entry function.
   \return
 */
-void main_entry(void) {
+void main_entry(void)
+{
     HAL_USART_InitPrint(&huart1, GPR_UART1ClkSel_26M, uart_cntrl, 115200);
-    printf("Exemplo FreeRTOS com botão e LED\n");
+    printf("Exemplo FreeRTOS com botão e LED - E Filas\n");
 
-    // Inicialização do GPIO
-    pad_config_t padConfig;
-    gpio_pin_config_t gpioConfig;  // ✅ DECLARAÇÃO CORRETA
-
-    PAD_GetDefaultConfig(&padConfig);
-    padConfig.mux = PAD_MuxAlt0;
-
-    // Botão GPIO10 (entrada com pull-up)
-    PAD_SetPinConfig(CUSTOM_BUTTON_PAD_ID, &padConfig);
-    PAD_SetPinPullConfig(CUSTOM_BUTTON_PAD_ID, PAD_InternalPullUp);
-    gpioConfig.pinDirection = GPIO_DirectionInput;
-    gpioConfig.misc.interruptConfig = GPIO_InterruptDisabled;
-    GPIO_PinConfig(CUSTOM_BUTTON_INSTANCE, CUSTOM_BUTTON_PIN, &gpioConfig);
-
-    // LED GPIO3 (saída)
-    PAD_SetPinConfig(BLUE_LED_PAD_ID, &padConfig);
-    PAD_SetPinPullConfig(BLUE_LED_PAD_ID, PAD_AutoPull);
-    gpioConfig.pinDirection = GPIO_DirectionOutput;
-    gpioConfig.misc.initOutput = LED_OFF;
-    GPIO_PinConfig(BLUE_LED_INSTANCE, BLUE_LED_PIN, &gpioConfig);
+    init_button();
+    init_led();
 
     slpManNormalIOVoltSet(IOVOLT_3_30V);
+
+    xButtonQueue = xQueueCreate(1, sizeof(bool));
+    if (xButtonQueue == NULL)
+    {
+        printf("Erro ao criar a fila\n");
+        while (1);
+    }
 
     xTaskCreate(Task1, "ButtonRead", 128, NULL, 1, NULL);
     xTaskCreate(Task2, "LedControl", 128, NULL, 1, NULL);
